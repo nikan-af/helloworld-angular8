7a266b8c750f20c386d29b21872637f4a26ed987
nikan-af <>
2021-03-29 20:29:52 -0400

    Initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'helloworld'`, () => {
+  it(`should have as title 'Hello World'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('helloworld');
+    expect(app.title).toEqual('Hello World');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('helloworld app is running!');
+    expect(compiled.querySelector('h2').textContent).toContain('Hello World');
   });
 });
